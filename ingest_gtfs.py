5b4558f998f2a265555d8527f525ec7b3c4a1586
IshwarBIMAECOM <ishwarprasad.balaji@aecom.com>
2026-04-02 15:42:08 -0400

    fix: correct DATABRICKS_HOST environment variable name

--- ingest_gtfs.py
+++ ingest_gtfs.py
@@ -27,7 +27,7 @@ FEEDS = {
 
 def get_connection():
     return sql.connect(
-        server_hostname = DATABRICKS_SERVER_HOSTNAME,
+        server_hostname = DATABRICKS_HOST,
         http_path       = DATABRICKS_HTTP_PATH,
         access_token    = DATABRICKS_TOKEN
     )
